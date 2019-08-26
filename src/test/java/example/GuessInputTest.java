af86aaa0e67cf12048ea79d9a425759624977abb
wenmingwy <2377812120@qq.com>
2019-08-26 23:54:15 +0800

    Test02

---
+++ src/test/java/example/GuessInputTest.java
@@ -0,0 +1,25 @@
+package example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class GuessInputTest {
+	
+	@Test
+	public void shoud_return_int_array_when_call_RandomIntArray_given_none() {
+//		given
+//		when
+//		then
+	}
+
+
+	@Test
+	public void should_return_true_or_false_when_call_VerifyInputIsValid_isValid_given_string() {
+//		given
+//		when
+//		then
+	}
+ 
+	
+}
