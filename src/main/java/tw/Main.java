b81a51b077f512d134089aa836d04fcdbd09d96f
wenmingwy <2377812120@qq.com>
2019-08-26 23:06:19 +0800

    add a class

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -1,5 +1,10 @@
 package tw;
 
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.omg.PortableServer.ID_ASSIGNMENT_POLICY_ID;
+
 import tw.commands.GuessInputCommand;
 
 /**
@@ -8,11 +13,14 @@ import tw.commands.GuessInputCommand;
 public class Main {
 
     private static final int RUN_TIMES = 6;
-
+    static String str="1234";
+    static outputMessage outputmessage = new outputMessage() ;
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < RUN_TIMES; i++) {
-            String input = new GuessInputCommand().input();
-            System.out.println(input);
-        }
-    }
+        String result = "";
+     for (int i = 0; i < RUN_TIMES; i++) {
+            String input = new GuessInputCommand().input();   
+            result = outputmessage.resultMessage(input,str);
+            System.out.println(result);
+        }     
+     }
 }
---
+++ src/main/java/tw/outputMessage.java
@@ -0,0 +1,27 @@
+package tw;
+
+public  class  outputMessage {
+	
+	String resultMessage (String input,String str) {
+		if(input.equals(str)) {
+			return "right";
+		}
+		else {
+			int matching = 0;
+			int matchingButPositionError = 0;
+			char [] stringInput = input.toCharArray();
+			char [] stringStr = str.toCharArray();
+			for(int i=0;i<4;i++) {
+				for(int j=0;j<4;j++) {
+					if(stringInput[i]==stringStr[j] && i==j) {
+						matching += 1;
+					}
+					else if(stringInput[i]==stringStr[j] && i!=j) {
+						matchingButPositionError += 1;
+					}
+				}
+			}
+	   return matching + "A" + matchingButPositionError + "B";
+	  }
+   }
+}
